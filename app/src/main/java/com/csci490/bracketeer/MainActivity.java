a5621c9f4708bd0ff97fb7132ea5fc21c025833f
Logan Schmidt <loganschmidt94@gmail.com>
2019-09-26 21:17:41 -0700

    Added second activity and adjusted layouts

--- app/src/main/java/com/csci490/bracketeer/MainActivity.java
+++ app/src/main/java/com/csci490/bracketeer/MainActivity.java
@@ -2,7 +2,9 @@ package com.csci490.bracketeer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the new tournament button is pressed */
+    public void createNewTournament(View view) {
+        Intent intent = new Intent(this, NewTournamentActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/csci490/bracketeer/NewTournamentActivity.java
@@ -0,0 +1,14 @@
+package com.csci490.bracketeer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewTournamentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_tournament);
+    }
+}
