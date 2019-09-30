2c7c4457b0120107008fb7f23cce0465fc4e4ff0
Logan Schmidt <loganschmidt94@gmail.com>
2019-09-30 13:29:23 -0700

    Restructured main activity layout, added stub toast to Load Tournament button

--- app/src/main/java/com/csci490/bracketeer/MainActivity.java
+++ app/src/main/java/com/csci490/bracketeer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +19,9 @@ public class MainActivity extends AppCompatActivity {
     public void createNewTournament(View view) {
         Intent intent = new Intent(this, NewTournamentActivity.class);
         startActivity(intent);
+    }
+
+    public void loadTournament(View view) {
+        Toast.makeText(this, "You clicked Load Tournament. This feature will be implemented soon", Toast.LENGTH_SHORT).show();
     }
 }
